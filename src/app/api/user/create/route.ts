67040bdea476375efcc50305ca71c575513ebd9b
Ilya Murzalev <murzalev@a2design.biz>
2024-08-09 16:54:52 +0600

    Set default max energy value on user creating

--- src/app/api/user/create/route.ts
+++ src/app/api/user/create/route.ts
@@ -16,7 +16,7 @@ POST http://localhost:3000/api/user/create
 
 export async function POST(req: NextRequest) {
     try {
-        const { id, first_name, last_name, username, referal_id } = await req.json();
+        const { id, first_name, last_name, username, referal_id, maxenergy = 1000 } = await req.json();
 
         if (!id || !first_name) {
             return NextResponse.json({ error: "Invalid input data" }, { status: 400 });
@@ -45,6 +45,7 @@ export async function POST(req: NextRequest) {
                     scores: 0,
                     username: username,
                     referal_id: referal_id,
+                    maxenergy: maxenergy,
 
                 }]);
 
