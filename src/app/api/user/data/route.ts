0088525c379a9ef4f49cd4949892c3f49129fb80
Oleg Bubelich <bubelich@a2design.biz>
2024-08-15 16:52:42 +0300

    Revert testing

--- src/app/api/user/data/route.ts
+++ src/app/api/user/data/route.ts
@@ -1,34 +1,35 @@
 import supabase from "@/db/supabase"
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
     try {
         // Получение id пользователя из запроса
-        // const { searchParams } = new URL(req.url);
-        // const id = searchParams.get('id');
+        const { searchParams } = new URL(req.url);
+        const id = searchParams.get('id');
 
-        // if (!id) {
-        //     return Response.json({ error: "Missing user ID" }, { status: 400 });
-        // }
+        if (!id) {
+            return NextResponse.json({ error: "Missing user ID" }, { status: 400 });
+        }
 
         // Получение данных пользователя из базы данных
         const { data: user, error: fetchError } = await supabase
             .from('users')
             .select('*')
-            .eq('id', 707837995)
+            .eq('id', id)
             .single();
 
         if (fetchError) {
             console.error("Failed to fetch user:", fetchError);
-            return Response.json({ error: "Failed to fetch user" }, { status: 500 });
+            return NextResponse.json({ error: "Failed to fetch user" }, { status: 500 });
         }
 
         if (!user) {
-            return Response.json({ error: "User not found" }, { status: 404 });
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
         }
 
-        return Response.json({ user }, { status: 200 });
+        return NextResponse.json({ user }, { status: 200 });
     } catch (error) {
         console.error("Error processing request:", error);
-        return Response.json({ error: "Internal server error" }, { status: 500 });
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
     }
 }
