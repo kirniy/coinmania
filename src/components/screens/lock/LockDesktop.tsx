7c12341c2433e00a5cae906cfed058868ffdcd82
Oleg Bubelich <bubelich@a2design.biz>
2024-08-16 12:33:57 +0300

    FIx lock desktop

--- src/components/screens/lock/LockDesktop.tsx
+++ src/components/screens/lock/LockDesktop.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-export default function LockDesctop() {
+export default function LockDesktop() {
   return (
       <div className="h-screen w-full bg-black text-center flex items-center justify-center pt-4">
           <div className="flex flex-col items-center justify-center">
