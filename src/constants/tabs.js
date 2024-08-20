98daea1c1ad7d7baf35f653e31ad087aa4c30db9
Ilya Murzalev <murzalev@a2design.biz>
2024-08-20 13:02:32 +0600

    COIN-4: Updated tabs (renamed "Bonus" to "Earn"), updated tabs order

--- src/constants/tabs.js
+++ src/constants/tabs.js
@@ -1,6 +1,6 @@
 export const TABS = [
   { name: 'Home', icon: '🏠', color: '#f8cc46', path: '/' },
+  { name: 'Earn', icon: '💰', color: '#842221', path: '/bonus' },
   { name: 'Game', icon: '🎮', color: '#5c35c5', path: '/game' },
-  { name: 'Bonus', icon: '🎁', color: '#842221', path: '/bonus' },
   { name: 'Frens', icon: '👥', color: '#2596be', path: '/frens' },
 ];
