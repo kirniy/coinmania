80821419576f15e8c4103db04c52625622c30075
Ilya Murzalev <murzalev@a2design.biz>
2024-08-21 17:24:12 +0600

    COIN-4: Created constants for "Earn" tab

---
+++ src/constants/earn.ts
@@ -0,0 +1,22 @@
+import type { Booster } from "@/types/boosters"
+
+export const BOOSTERS : Booster[] = [
+    {
+        name: 'Full Tank',
+        slug: 'full_tank',
+        description: 'Восстановление полной энергии',
+        duration: 0,
+        maxUsePerDay: 3,
+        action: 'resetEnergy',
+        bgColor: '#f8cc46',
+    },
+    {
+        name: 'Tap Boost',
+        slug: 'tap_boost',
+        description: 'Множитель x5 на 20 секунд',
+        duration: 20,
+        maxUsePerDay: 3,
+        action: 'tapBoost',
+        bgColor: '#ff7f50',
+    }
+]
