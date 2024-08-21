6918f330e1883be719273c3dfe140c46778b732b
Ilya Murzalev <murzalev@a2design.biz>
2024-08-21 13:39:01 +0600

    COIN-4: Created types folder, added user data type

---
+++ src/types/user.ts
@@ -0,0 +1,16 @@
+export interface UserData {
+    id: string;
+    first_name: string;
+    last_name: string;
+    username: string | null;
+    last_login_time: string;
+    energy: number | null;
+    maxenergy: number;
+    scores: number | null;
+    energyresettime: string;
+    daily_spin_count: number;
+    last_spin_time: string | null;
+    daily_tap_boost_count: number;
+    last_tap_boost_time: string | null;
+    referal_id: string | null;
+};
