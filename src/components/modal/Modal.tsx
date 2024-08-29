f3eaa4e6ea2b71a122a980caef69079a7cadcfef
Egor Kalmykov <kalmykov@a2design.biz>
2024-08-29 11:55:27 +0300

    COIN-24: no-scroll fix (perhaps)

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,13 +1,18 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useEffect } from "react";
 
 interface ModalProps extends PropsWithChildren {
     onClose: () => void;
 }
 
 export function Modal({ children, onClose }: ModalProps) {
+    useEffect(() => {
+        const preventDefault = (e: Event) => e.preventDefault();
+        document.removeEventListener('touchmove', preventDefault);
+        return () => document.addEventListener('touchmove', preventDefault, { passive: false });
+    })
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 w-full h-full bg-black bg-opacity-80">
-        <div className="relative left-[50%] top-[43%] translate-x-[-50%] translate-y-[-50%]">
+        <div className="relative left-[50%] top-[43%] translate-x-[-50%] translate-y-[-50%] px-4">
             <div className="fixed right-6 -top-6">
                 <button className="" onClick={onClose}>✕</button>
             </div>
