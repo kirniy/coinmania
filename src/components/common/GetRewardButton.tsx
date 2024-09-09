f9d52b361cb3c553145224fb4d4cc89009bbce7b
Ilya Murzalev <murzalev@a2design.biz>
2024-09-09 13:33:13 +0600

    COIN-55: Updated GetRewardButton component, added received state

--- src/components/common/GetRewardButton.tsx
+++ src/components/common/GetRewardButton.tsx
@@ -1,23 +1,41 @@
 import React from "react";
+import { Check } from "lucide-react";
 
 type GetRewardButtonProps = {
     children: React.ReactNode,
-    onClick: () => void,
+    onClick?: () => void,
+    isReceived?: boolean,
 }
 
-const GetRewardButton: React.FC<GetRewardButtonProps> = ({ children, onClick }) => {
+const GetRewardButton: React.FC<GetRewardButtonProps> = ({ children, onClick, isReceived = false }) => {
 
     return (
-        <button
-            className="
-                bg-[#f8cc46] rounded-full w-auto max-w-[500px] px-4 py-2 text-sm font-bold text-black
-                cursor-pointer flex items-center justify-center shadow-md transition-transform duration-300
-                ease-in-out transform scale-100 hover:scale-105
-            "
-            onClick={onClick}
-        >
-            {children}
-        </button>
+        <>
+            {isReceived && 
+                <div
+                    className="
+                        bg-[#00b600] rounded-full w-auto max-w-[500px] px-4 py-1 text-xs font-bold text-black
+                        flex gap-1 items-center justify-center shadow-md hover:cursor-default                     
+                    "
+                >
+                    <Check width={15} height={15} />
+                    {children}
+                </div>
+            }
+
+            {!isReceived &&
+                <button
+                    className="
+                        bg-[#f8cc46] rounded-full w-auto max-w-[500px] px-4 py-1 text-xs font-bold text-black
+                        cursor-pointer flex items-center justify-center shadow-md transition-transform duration-300
+                        ease-in-out transform scale-100 hover:scale-105
+                    "
+                    onClick={onClick}
+                >
+                    {children}
+                </button>
+            }
+        </>
     )
 }
 
