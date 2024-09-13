ff36c5153154b2ea2a6d9f3e7299bb4cfa58efab
Ilya Murzalev <murzalev@a2design.biz>
2024-09-13 17:51:38 +0600

    COIN-45: Updated modal component styles

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -23,13 +23,19 @@ export function Modal({ children, onClose }: ModalProps) {
         };
     })
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4" onClick={onClose} style={{zIndex: 200}}>
-    <div className="bg-gray-800 rounded-xl p-6 w-full max-w-sm max-h-[70vh] overflow-y-auto relative" onClick={e => e.stopPropagation()}>
-      <button className="absolute top-2 right-2 text-gray-400 hover:text-white" onClick={onClose}>
-        <X size={24} />
-      </button>
-      {children}
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-start justify-center p-4 pt-0 backdrop-blur-[5px]"
+      onClick={onClose}
+      style={{zIndex: 200}}
+    >
+      <div className="bg-gray-800 rounded-xl p-6 w-full max-w-sm max-h-[90vh] scrollbar-hide overflow-y-auto relative" onClick={e => e.stopPropagation()}>
+        <button className="absolute top-2 right-2 text-gray-400 hover:text-white" onClick={onClose}>
+          <X size={24} />
+        </button>
+        <div className='flex flex-col gap-4 text-white'>
+          {children}
+        </div>
+      </div>
     </div>
-  </div>
   );
 }
